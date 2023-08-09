13c4179f47397741a52f77d880ce1d050a53a783
agusgonz <agustingendeiza@gmail.com>
2023-08-09 14:36:45 -0300

    Fix typo and add better error handler

--- src/app/api/register/route.tsx
+++ src/app/api/register/route.tsx
@@ -5,24 +5,30 @@ import bcrypt from "bcrypt"
 export async function POST(request: NextRequest) {
 	const body = await request.json()
 	const { name, email, password } = body
-	const userExist = await prisma.user.findUnique({
-		where: {
-			email,
-		},
-	})
-	if (userExist) {
-		return new NextResponse("Email already in user", {
+	try {
+		const userExist = await prisma.user.findUnique({
+			where: {
+				email,
+			},
+		})
+		if (userExist) {
+			return new NextResponse("Email already in use", {
+				status: 400,
+			})
+		}
+		const hashedPassword = await bcrypt.hash(password, 10)
+
+		const user = await prisma.user.create({
+			data: {
+				name,
+				email,
+				password: hashedPassword,
+			},
+		})
+		return NextResponse.json(user)
+	} catch (err) {
+		return new NextResponse("Something when wrong", {
 			status: 400,
 		})
 	}
-	const hashedPassword = await bcrypt.hash(password, 10)
-
-	const user = await prisma.user.create({
-		data: {
-			name,
-			email,
-			password: hashedPassword,
-		},
-	})
-	return NextResponse.json(user)
 }
