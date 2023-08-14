d5d4ae54188097345c510aaee7ad7f1c28b01ef8
agusgonz <agustingendeiza@gmail.com>
2023-08-14 14:48:17 -0300

    Add loading state to avatar image

--- src/components/AvatarIcon.tsx
+++ src/components/AvatarIcon.tsx
@@ -1,9 +1,11 @@
-import { FC } from "react"
+"use client"
+import { FC, useState } from "react"
 import {
 	Avatar,
 	AvatarFallback,
 	AvatarImage,
 } from "./ui/Avatar"
+import Spinner from "./Spinner"
 
 interface AvatarIconProps
 	extends React.HTMLAttributes<HTMLDivElement> {
@@ -15,14 +17,21 @@ const AvatarIcon: FC<AvatarIconProps> = ({
 	image,
 	isBig,
 }) => {
+	const [imageLoaded, setImageLoaded] = useState(false)
+
 	return (
 		<Avatar
 			className={`${
 				isBig ? "h-40 w-40" : "h-6 w-6"
-			} border-2 border-_blue`}
+			} border-2 border-_blue bg-_blue`}
 		>
-			<AvatarImage src={image ? image : "/default.jpg"} />
-			<AvatarFallback>Avatar Icon</AvatarFallback>
+			{ !imageLoaded && <div className="w-full h-full flex items-center justify-center"><Spinner/></div>}
+			
+			<AvatarImage
+				src={image ? image : "/default.jpg"}
+				className={`object-cover ${imageLoaded ? "w-full " : "w-[1px] h-[1px]"}`}
+				onLoad={() => setImageLoaded(true)}
+			/>
 		</Avatar>
 	)
 }
