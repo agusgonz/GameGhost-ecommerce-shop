408d1a7d2ffac25ad9cffae46da75fa1eb781f8e
agusgonz <agustingendeiza@gmail.com>
2023-08-14 14:01:17 -0300

    Add spinner style editing outside component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils/utils"
 import { Loader2 } from "lucide-react"
 import React from "react"
 
@@ -6,7 +7,9 @@ import { FC } from "react"
 interface SpinnerProps
 	extends React.HTMLAttributes<HTMLDivElement> {}
 
-const Spinner: FC<SpinnerProps> = ({}) => {
-	return <Loader2 className="animate-spin" />
+const Spinner: FC<SpinnerProps> = ({ className }) => {
+	return (
+		<Loader2 className={cn("animate-spin", className)} />
+	)
 }
 export default Spinner
