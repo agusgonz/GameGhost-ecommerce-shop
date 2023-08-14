81ecc403eeb671a4cbd712c4e8dcde1eaea857a9
agusgonz <agustingendeiza@gmail.com>
2023-08-14 14:50:13 -0300

    Fix loading and unauthenticated messagge to be in the center

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,8 +3,10 @@ import Spinner from "./Spinner"
 
 export default function Loading() {
 	return (
-		<div className="w-full h-full flex items-center justify-center ">
-			<Spinner />
-		</div>
+		<>
+			<div className="absolute top-1/2 left-1/2 flex items-center justify-center -translate-x-1/2 -translate-y-1/2">
+				<Spinner />
+			</div>
+		</>
 	)
 }
--- src/components/UnauthenticatedFrame.tsx
+++ src/components/UnauthenticatedFrame.tsx
@@ -9,7 +9,7 @@ const UnauthenticatedFrame: FC<
 	UnauthenticatedFrameProps
 > = ({}) => {
 	return (
-		<div className="flex flex-col gap-2 justify-center items-center pt-5">
+		<div className="flex flex-col gap-2 justify-center items-center pt-5 text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 			<h1>You have to be authenticated to see this page</h1>
 			<Link
 				className={
