577cb313053a1e33f74b6ec5e40c234f1ecb411d
agusgonz <agustingendeiza@gmail.com>
2023-08-16 15:29:52 -0300

    Add loading state to products in main page

--- src/components/CategorySection/index.tsx
+++ src/components/CategorySection/index.tsx
@@ -7,14 +7,20 @@ import { useEffect, useState } from "react"
 import { useSession } from "next-auth/react"
 import Loading from "@/components/Loading"
 import FadeInAnimation from "../FadeInAnimation"
+import Spinner from "../Spinner"
 
 export default function CategorySection() {
 	const { data: session, status } = useSession()
 	const [categories, setCategories] = useState<Category[]>()
+	const [loadingProducts, setLoadingProducts] =
+		useState(true)
 
 	useEffect(() => {
 		getCategories()
-			.then(res => setCategories(res.data))
+			.then(res => {
+				setCategories(res.data)
+				setLoadingProducts(false)
+			})
 			.catch(err => {
 				toast({
 					description: err.response.data,
@@ -22,6 +28,17 @@ export default function CategorySection() {
 				})
 			})
 	}, [])
+
+	if (loadingProducts) {
+		return (
+			<FadeInAnimation>
+				<div className="w-full h-32 flex justify-center items-center ">
+					<p className="pr-2">Loading Products</p>
+					<Spinner />
+				</div>
+			</FadeInAnimation>
+		)
+	}
 
 	return (
 		<>
